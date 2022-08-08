82006d1920593c39e737bece99b5e6523d0ef646
anasshafiq <anas.shafiq10@gmail.com>
2022-08-08 18:52:12 +0500

    added oop dir

---
+++ oop/Methods.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class Methods {
+
+    public int add(int no1, int no2) {
+        return no1 + no2;
+    }
+
+    public static void main(String[] args) {
+        Methods method = new Methods();
+        System.out.println(method.add(45, 1));
+
+    }
+
+}
