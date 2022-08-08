cc45013795fe0cce6dfebfe6f26748b10a9b3174
anasshafiq <anas.shafiq10@gmail.com>
2022-08-08 16:56:59 +0500

    added practice.java :recycle:

---
+++ Practice.java
@@ -0,0 +1,23 @@
+public class Practice 
+{
+    public static void main (String[]args)
+    {
+        System.out.println("Hello This is a practice program ");
+        int a = 30;
+        int b = 40;
+        System.out.println("The fisrt number given is = "+a);
+        System.out.println("The second number given is = "+b);
+        int sum = a+b;
+        System.out.println("The sum of the above given numbers are = " + sum );
+        System.out.println("now we will swap there places");
+        a= a+b;
+        b= a-b;
+        a= a-b;
+        System.out.println("Now A = "+a);
+        System.out.println("Now B = "+b);
+        System.out.println( ("The Sum of both is =")+((a)+(b)));
+    
+    }
+
+    
+}
